7ed0ab35c52cfe5b467d85c3749156d2b993f03c
Bjorkbat <bjorkbat@gmail.com>
2016-07-19 12:28:26 -0600

    Removed some of the merge code

--- index.php
+++ index.php
@@ -1,16 +1,10 @@
-
-    
-=======
+<!DOCTYPE html>
+<html>
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
->>>>>>> origin/master
 </head>
 <body>
 
 </body>
-<<<<<<< HEAD
 </html>
-=======
-</html>
->>>>>>> origin/master

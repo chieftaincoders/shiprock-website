451ad699288e98802057e1d26eb60f293a91e5b7
Danyel Johnson <sisnaajin9@gmail.com>
2016-07-20 07:58:24 -0700

    added files

---
+++ Athletics.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <?php require_once("navbar.php")?>
+    <?php require_once("lib/head-utils.php")?>
+</head>
+<body>
+<h1>Athletics</h1>
+
+
+
+
+</body>
+</html>

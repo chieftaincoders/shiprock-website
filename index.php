9fe362628bc315b03228be68530a23b02629d7e1
ccoder23 <pettadam347@gmail.com>
2016-07-20 09:11:37 -0600

    Added a jrotc file with some info about it. Put in a club page directory with all of the clubs in it.

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 <html>
 <head>
 
-    <?php require_once("navbar.php")?>
-    <?php require_once("lib/head-utils.php")?>
+    <?php require_once("navbar.php") ?>
+    <?php require_once("lib/head-utils.php") ?>
 </head>
 <body>
 <h1>hello</h1>
---
+++ lib/Club Pages/ffa.php
@@ -0,0 +1,2 @@
+<h1>FFA</h1>
+    <p>FFA which stand for Future Farmers of America, is a club where students get to </p>

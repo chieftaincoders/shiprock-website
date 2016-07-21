c950f2600234a5fb0cbc285a9c46e8f0b05f77c4
ccoder23 <pettadam347@gmail.com>
2016-07-21 14:55:58 -0600

    Added more to JROTC, and Band, created clubs directory, created lib directory in clubs, created head-utils.php and css within the lib directory that was made in the clubs directory.

---
+++ lib/clubs/band.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <?php require_once("lib/head-utils.php") ?>
+</head>
+<body>
+
+<section class="rotcmodule parallax parallax">
+    <div class="container fadeInDown animated">
+        <div class="col-md-offset-2 col-md-8 col-md-offset-2">
+            <h1 class="band">Shiprock High School Band</h1>
+            <p class="bandp"></p>
+        </div>
+    </div>
+</section>
+
+<div class="background">
+    <div class="container">
+
+        <h3>Paul Magnison</h3>
+        <p>Paul Magnison is the Shiprock High School's band director. </p>
+
+        <h3>Marching Season</h3>
+            <p>
+                Marching Season starts in August and ends around November, during this time students learn a marching drill, and memorize
+                song which are usually three parts long. When the songs and positions are memorized, they perform a halftime show for every football game.
+                With Marching Music, band students also learn pep music, which is played at every home football games along with a halftime show
+                where they perform their Marching songs, and routine. The band plays at competitions, where they perform their show along with,
+                competing against other Highschool Marching bands from the district. During marching the band performs asong in the Annual Shiprock
+                Fair Parade, Marching from one end of the town, to the other end. Shiprock Highschool also plays in the Farmington Christmas parade.
+            </p>
+
+        <h3>General Band and Jazz Band</h3>
+            <p class="bandp">
+                When Marching Season is over, Magnison gives students new music to learn, with a few songs changed for certain events.
+                Jazz band is a small band with a select group of band members, and learn jazz music, and general band music at the same time.
+                Both of the bands performs in concerts that anyone can watch, and joins competitions against other schools. The competitions,
+                require the band to perform their music, read and play completely new music while being jugded for both performances.
+            </p>
+
+        <h3>Music in The Park Fesitvals</h3>
+            <p>
+                The biggest event is the Music in the Park Festivals, and is a trip tht last a few days, and is fun
+                for the band students. The trip for the 2015-2016 band was to California, and the visited amusement parks, and beaches including
+                Disneyland for the 50th anniversary. The band earned second place for both jazz and general band, along with the    trophy, which
+                is awarded for being respectful, which represents our school, and is considered one of the most important trophies given out.
+                The trip is not free, and students must pay a huge amount of money to be able to go, and it is possible for the    to pay part
+                of the trip and lower the price for the students. Fund raisers are held throughout the year, and gives students a chance to
+                lower the price of the payment.
+        </p>
+
+        <h3>Comments About SHS Band</h3>
+            <p class="bandp">
+                BandBandBandBandBandBandBandBandBandBandBandBand
+            </p>
+    </div>
+</div>
+</body>
+</html>
---
+++ lib/clubs/ffa.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+</head>
+<body>
+
+</body>
+</html>
+
+<div class="container">
+    <h1>Future Farmers of America</h1>
+        <p>
+            Future Farmers of America, also known as FFA, is a club that takes students in the agriculture classes and
+            uses their skill for events throughout the year.
+        </p>
+
+    <h2></h2>
+        <p>
+
+        </p>
+</div>

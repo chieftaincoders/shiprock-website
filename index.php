ba5d60dd6141df1b8fcd9d88edde8ca42ce1ae5c
Danyel Johnson <sisnaajin9@gmail.com>
2016-07-26 09:09:10 -0700

    took things off in index

--- index.php
+++ index.php
@@ -8,17 +8,7 @@ U<!DOCTYPE html>
 <body>
 
 <div class="hero">
-
-    <div class="parallax-layer layer-6"></div>
-    <div class="parallax-layer layer-5"></div>
-    <div class="parallax-layer layer-4"></div>
-    <div class="parallax-layer bike-1"></div>
-    <div class="parallax-layer bike-2"></div>
-    <div class="parallax-layer layer-3"></div>
-    <div class="parallax-layer layer-2"></div>
-    <div class="parallax-layer layer-1"></div>
-    <div class="logo">
-        <img src="shsPic.jpg" alt=""  />
+        <img src="lib/images/shsPic.jpg" alt=""  />
         <div class="light-bg1">
             <h1>Shiprock High School</h1>
             <h2>once a chieftain always a chieftain</h2>
@@ -30,9 +20,6 @@ U<!DOCTYPE html>
 
 
 
-<div class="dark-bg">
-    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/24650/Group_5.svg" alt="" />
-</div>
 
 <div class="light-bg">
     <p class="extra"></p>
@@ -42,19 +29,7 @@ U<!DOCTYPE html>
 </div>
 
 
-<script>
-    WebFontConfig = {
-        google: { families: [ 'Lato:400,300,300italic,400italic,700,700italic,900,900italic:latin' ] }
-    };
-    (function() {
-        var wf = document.createElement('script');
-        wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-        wf.type = 'text/javascript';
-        wf.async = 'true';
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(wf, s);
-    })();
-</script>
+
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 <script src="lib/js/tweets.js"></script>

8e74a2e781e4b96ac73358a59be998ad82847bd8
ccoder23 <pettadam347@gmail.com>
2016-08-10 09:11:41 -0600

    made one club page. deleted the rest. and finished the band page.

--- clubs.php
+++ clubs.php
@@ -15,12 +15,11 @@
     </div>
 </section>
 
-<div class="clubsbackground">
-    <div class="container">
-
+ <div class="container">
+     <div class="clubsbackground">
         <div class="col-md-6">
-            <h3 class="cheerh3">Cheer Leaders</h3>
-                <p class="cheerp1">
+            <h3 class="clubsh3">Cheer Leaders</h3>
+                <p class="clubsp">
                     The Shiprock High school cheer leaders cheer for basketball and football games, each sport having its own specific cheer,
                     with some cheers that are used throughout both seasons. Cheer leaders of 2015-2016 were mostly seniors and have graduated,
                     the cheer leaders for 2016-2017 will need more new members so students are encouraged to join.
@@ -28,8 +27,8 @@
         </div>
 
         <div class="col-md-offset-6 col-md-6">
-            <h3 class="chessh3">Chess</h3>
-                    <p class="chessp">
+            <h3 class="clubsh3">Chess</h3>
+                    <p class="clubsp">
                         Chess club is where students get together in the morning during breakfast and during lunch almost daily. The student that
                         are in this club play against each and have competitionswith each other. The SHS Librarian, started this club and decides
                         when it begins.
@@ -37,30 +36,41 @@
         </div>
 
         <div class="col-md-6">
-            <h3 class="dramah3">Drama</h3>
-                <p class="dramap">
-
+            <h3 class="clubsh3">Drama</h3>
+                <p class="clubsp">
+                    The SHS Drama Club
                 </p>
         </div>
 
         <div class="col-md-offset-6 col-md-6">
-            <h3 class="nhsh3">National Honors Society</h3>
-                <p class="nhsp">
-
+            <h3 class="clubsh3">Future Farmers of America</h3>
+                <p class="clubsp">
+                    FFA is a club that pushes you to learn about the environment, and agricultural uses. FFA Members compete against school around the
+                    district and out of State. The competitions are based around agriculture questions and scenarios, mixed with math
+                    questions, and general questions about the subject. Members travel for the competitions or conventions, where they get the chance
+                    to meet other FFA members. FFA is creating a better community for all of the inhabitants, and teaches about agricultural
+                    uses.
                 </p>
         </div>
 
         <div class="col-md-6">
-            <h3 class="rioth3">Riot Squad</h3>
-                <p class="p">
+            <h3 class="clubsh3">National Honors Society</h3>
+                <p class="clubsp">
+                    In the National Honors Society students
+                </p>
+        </div>
+
+        <div class="col-md-offset-6 col-md-6">
+            <h3 class="clubsh3">Riot Squad</h3>
+                <p class="clubsp">
                     The SHS Riot Squad attends the home football and basketball games to cheer on the SHS Teams. The Riot Squad
                     enters the games for free, and have a place reserved for them to sit and support the teams as they play.
                 </p>
         </div>
 
-        <div class="col-md-offset-6 col-md-6">
-            <h3 class="councilh3">Student Council</h3>
-                <p class="councilp">
+        <div class="col-md-6">
+            <h3 class="clubsh3">Student Council</h3>
+                <p class="clubsp">
                     The Student Council is a club that you are elected by your peers to be your grade's president. The presidents from each
                     grade get together and discuss fund-raisers that will rease money for student events like dances and graduation.
                 </p>
--- schedules.php
+++ schedules.php
@@ -9,37 +9,42 @@
 <h1 id="schedule">Schedules</h1>
 
 
-<div class="container">
-    <h2 id="bell">Bell Schedules</h2>
+ <div class="schedbackground">
+     <div class="container">
+        <h2 id="bell">Bell Schedules</h2>
 
-            <div class="col-md-6">
-                <h3 id="regmon">Monday, Wednesday, Friday</h3>
-                    <img id="schedulepics" src="lib/images/shsbellschedule.png">
-            </div>
+                <div class="col-md-6">
+    <!--                <h3 id="regmon">Monday, Wednesday, Friday</h3>-->
+                        <img id="schedulepics" src="lib/images/shsbellschedule.png">
+                </div>
 
-            <div class="col-md-6">
-                <h3 id="regtues">Tuesday and Thusday</h3>
-                    <img id="schedulepics" src="lib/images/shsbellscheduletuethur.png">
-            </div>
+                <div class="col-md-6">
+    <!--                <h3 id="regtues">Tuesday and Thusday</h3>-->
+                        <img id="schedulepics" src="lib/images/shsbellscheduletuethur.png">
+                </div>
 
-            <div class="col-md-6">
-                <h3 id="early">Early Release</h3>
-                    <img id="schedulepics" src="lib/images/shsbellscheduleearly.png">
-            </div>
+                <div class="col-md-6">
+    <!--                <h3 id="early">Early Release</h3>-->
+                        <img id="schedulepics" src="lib/images/shsbellscheduleearly.png">
+                </div>
 
-            <div class="col-md-6">
-                <h3 id="delay">Two Hour Delay</h3>
-                    <img id="schedulepics" src="lib/images/shsbellscheduledelay.png">
-            </div>
+                <div class="col-md-6">
+    <!--                <h3 id="delay">Two Hour Delay</h3>-->
+                        <img id="schedulepics" src="lib/images/shsbellscheduledelay.png">
+                </div>
+
+        <h2 class="district">District Calendar</h2>
+
+
+
+
+
+
+
+
+        <h2 class="monthly">Monthly School Calendar</h2>
+
+    </div>
 </div>
-
-<h2 class="district">District Calendar</h2>
-
-
-
-
-
-
-
-
-<h2 class="monthly">Monthly School Calendar</h2>
+</body>
+</html>

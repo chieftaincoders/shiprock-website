e7eaaef7cf723520298499add167cc410fd942a1
Danyel Johnson <sisnaajin9@gmail.com>
2016-08-10 08:10:24 -0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	index.php

---
+++ lib/sports/crosscountry.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/html">
+<head>
+
+    <?php require_once("navbar.php"); ?>
+    <?php require_once("lib/head-utils.php"); ?>
+
+
+</head>
+<body>
+
+
+
+
+
+
+</body>
+</html>

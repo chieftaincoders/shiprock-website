0519777abcbf3899f03e31424c1e14df5314ae74
Danyel Johnson <sisnaajin9@gmail.com>
2016-08-10 10:31:29 -0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	index.php

--- lib/Athletics.php
+++ lib/Athletics.php
@@ -1,14 +1,11 @@
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/html">
+<html>
 <head>
 
-    <?php require_once("navbar.php"); ?>
-    <?php require_once("lib/head-utils.php"); ?>
-
-
+    <?php require_once("navbar.php") ?>
+    <?php require_once("lib/head-utils.php")?>
 </head>
-<body>
-
+<body
 
 <div class="mainImage">
        <h2 class="heading">Shiprock High School</h2>
@@ -16,8 +13,6 @@
                and socio emotional skills they need to be successful in whatever path they pursue after high school.</p>
        <p><a href="#landHere" <a href="#" class="continue" data-0="@class:continue;" data-60p-top="@class:continue-gone;">Scroll Down</a></a></p>
 </div>
-
-
 
 
 

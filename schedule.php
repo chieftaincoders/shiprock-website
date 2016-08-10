54ea3626d4be300de5f8916fe3802096b8fe08ad
ccoder23 <pettadam347@gmail.com>
2016-08-10 11:19:33 -0600

    Worked on the clubs and schedule pages

--- schedule.php
+++ schedule.php
@@ -41,6 +41,10 @@
                 </div>
 
         <h2 class="district">District Calendar</h2>
+            <div class="col-md-offset-3 col-md-9">
+                <img src="lib/images/academiccalendar.png">
+            </div>
+
 
         <h2 class="monthly">Monthly School Calendar</h2>
 

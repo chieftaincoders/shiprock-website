0a1307eefeb47749696ac84f3c23eba3fac86878
Danyel Johnson <sisnaajin9@gmail.com>
2016-08-11 08:36:24 -0700

    added info to athletics page

---
+++ Athletics.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php require_once("lib/head-utils.php") ?>
+</head>
+<body
+
+<?php require_once("navbar.php") ?>
+
+
+<div class="mainImage">
+       <h2 class="heading">Shiprock High School</h2>
+       <p class="paragraph">Shiprock High School has a group of transformational educators who teach our students the academic
+               and socio emotional skills they need to be successful in whatever path they pursue after high school.</p>
+       <p><a href="#landHere" <a href="#" class="continue" data-0="@class:continue;" data-60p-top="@class:continue-gone;"
+        >Scroll Down</a></a></p>
+</div>
+
+<div class="mainImage">
+    <?php require_once("navBar.php"); ?>
+    <h2>Welcome to my Site.</h2>
+</div>
+
+
+<div class="hero-unit">
+    <h1>This a HERO PAGE</h1>
+    <h2>Just a plain hero page.</h2>
+    <p><a class="btn btn-primary btn-large" href="#">CLICK ME</a></p>
+</div>
+
+
+<div class="row">
+    <div class="col-md-12 inforDiv">
+        <div class="col-md-1"><i class="fa fa-cog infoIcons" aria-hidden="true"></i></div>
+        <div class="col-md-3">
+            <p> This is the new project heroPage</p>
+        </div>
+        <div class="col-md-1"><i class="fa fa-clock-o infoIcons" aria-hidden="true"></i></div>
+        <div class="col-md-3">
+            <p> This is the new project heroPage</p>
+        </div>
+        <div class="col-md-1"><i class="fa fa-clock-o infoIcons" aria-hidden="true"></i></div>
+        <div class="col-md-3">
+            <p> This is the new project heroPage</p>
+        </div>
+    </div>
+</div>
+
+<hr class="featurette-divider">
+
+<div class="row featurette">
+    <div class="col-md-7">
+        <h2 class="featurette-heading">This is where cool stuff goes.<br> <span class="text-muted">Really cool stuff ;).</span></h2>
+        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod
+            semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+    </div>
+    <div class="col-md-5">
+        <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="thisIsWhereAnImageGoes">
+    </div>
+</div>
+
+<hr class="featurette-divider">
+
+<div class="row featurette">
+    <div class="col-md-7 col-md-push-5">
+        <h2 class="featurette-heading">I know what you like. <br> <span class="text-muted">See for yourself.</span></h2>
+        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod
+            semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+    </div>
+    <div class="col-md-5 col-md-pull-7">
+        <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="thisIsWhereAnImageGoes">
+    </div>
+</div>
+
+<hr class="featurette-divider">
+
+<div class="row featurette">
+    <div class="col-md-7">
+        <h2 class="featurette-heading">And most importantly. <br> <span class="text-muted">SQUAD.</span></h2>
+        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod
+            semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+    </div>
+    <div class="col-md-5">
+        <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="thisIsWhereAnImageGoes">
+    </div>
+</div>
+
+<hr class="featurette-divider">
+
+
+
+
+
+</body>
+</html>
